6e6d3a780b56a1f99fff0b345cd0161238b0c890
raghu848 <dhimanvishal945@GMAIL.COM>
2026-05-21 12:27:25 +0530

    add tag manager

--- app/layout.jsx
+++ app/layout.jsx
@@ -86,11 +86,32 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${inter.variable} ${manrope.variable}`}>
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-ML5CHCLJ');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <meta name="theme-color" content="#0B1220" />
         <OrganizationSchema />
         <WebsiteSchema />
       </head>
       <body className={`${inter.className} antialiased text-text-secondary bg-background selection:bg-accent/30 selection:text-white`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-ML5CHCLJ"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <div className="relative min-h-screen">
           {children}
           <WhatsAppButton />
